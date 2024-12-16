9f4c8e0e92ad87676889db1e71598ad35fe48647
ayk <av.ahmetyasinkaraca@gmail.com>
2024-12-16 22:03:49 +0300

    basicsDeletedFile.cy

--- cypress/e2e/basics/navigation.cy.js
+++
@@ -1,7 +0,0 @@
-describe('Example: Visit and Find Elements', () => {
-    it('should visit the kitchen sink example', () => {
-        cy.visit('https://example.cypress.io');
-        cy.get('h1').should('contain', 'Kitchen Sink');
-    });
-});
-
