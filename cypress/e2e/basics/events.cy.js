2177df9691eb3b56314ae71870afbeb100035925
ayk <av.ahmetyasinkaraca@gmail.com>
2024-12-16 22:02:27 +0300

    basics2.cy

--- cypress/e2e/basics/events.cy.js
+++ cypress/e2e/basics/events.cy.js
@@ -1,10 +1,41 @@
 describe('Mouse and Keyboard Events', () => {
-    it('should hover over an element', () => {
-        cy.visit('https://example.cypress.io/commands/actions');
-        cy.get('.action-hover').trigger('mouseover').should('have.css', 'color', 'rgb(255, 255, 255)');
+
+    beforeEach(() => {
+        cy.visit('/commands/actions');
     });
 
-    it('should right-click on an element', () => {
-        cy.get('.rightclick-action').rightclick().should('have.class', 'context-menu');
+    it('should be typed', () => {
+        cy.get('.action-email').type('fake@email.com')
+        cy.get('.action-email').should('have.value', 'fake@email.com')
+    });
+
+    it('should be focused', () => {
+        cy.get('.action-focus').focus()
+        cy.get('.action-focus').should('have.class', 'focus')
+            .prev().should('have.attr', 'style', 'color: orange;')
+    });
+
+    it('clear text', () => {
+        cy.get('.action-clear').type('Clear this text')
+        cy.get('.action-clear').should('have.value', 'Clear this text')
+        cy.get('.action-clear').clear()
+        cy.get('.action-clear').should('have.value', '')
+    });
+
+    it('scroll to top', () => {
+        cy.scrollTo('top')
+    });
+
+
+    it('should be clicked', () => {
+        cy.get('.action-btn').click()
+        cy.get('.popover').should('be.visible')
+    });
+
+
+    it('should be double clicked', () => {
+        cy.get('.action-div').dblclick()
+        cy.get('.action-div').should('not.be.visible')
+        cy.get('.action-input-hidden').should('be.visible')
     });
 });
